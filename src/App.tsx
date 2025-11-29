5a098327dc51f29bc77c06252edf25037339416d
Arnovis Montero <arnovisua@gmail.com>
2025-11-29 16:48:25 -0500

    Fix: Switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import MoodSelector from './components/MoodSelector';
 import MovieGrid from './components/MovieGrid';
